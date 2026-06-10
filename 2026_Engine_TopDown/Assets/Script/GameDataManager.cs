ca0eef8c3df97d4b0beb71763cd1be01ddf027f9
wndmsrla0623-lu <wndmsrla0623@gmail.com>
2026-06-10 16:13:00 +0900

    스테이지 1 구성

---
+++ 2026_Engine_TopDown/Assets/Script/GameDataManager.cs
@@ -0,0 +1,113 @@
+using System.IO;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance;
+
+    public GameSettingData gameSettingData;
+
+    public SaveData saveData;
+
+    public int isTutorialFinished;
+
+
+    private string savePath;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            savePath = Application.persistentDataPath + "/saveData.json";
+
+            LoadJsonData();
+            LoadLayerPrefs();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetPlayerHP()
+    {
+        int baseHP = gameSettingData.startHp;
+        int bonusHp = gameSettingData.hpBonusPerDeath;
+
+        return baseHP + bonusHp * saveData.deathCount;
+    }
+
+    public int GetPlayerAttack()
+    {
+        int baseAttack = gameSettingData.startAttack;
+        int bonusAttack = gameSettingData.atkBonusPerDeath;
+        return baseAttack + bonusAttack * saveData.deathCount;
+    }
+
+    public void SaveGameResult()
+    {
+        saveData.deathCount++;
+
+        SaveJsonData();
+    }
+
+
+    public void SaveJsonData()
+    {
+        string json = JsonUtility.ToJson(saveData, true);
+        File.WriteAllText(savePath, json);
+
+        Debug.Log("JSON 저장 완료: " + savePath);
+    }
+
+    public void LoadjsonData()
+    {
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            saveData = JsonUtility.FromJson<SaveData>)json);
+        }
+        else
+        {
+            saveData = new SaveData();
+            SaveJsonData();
+        }
+    }
+
+    public void DeleteJsonData()
+    {
+        if (File.Exists(savePath))
+        {
+            File.Delete(savePath);
+        }
+
+        saveData = new SaveData();
+        SaveJsonData();
+
+        Debug.Log("Json 데이터 삭제 완료");
+    }
+
+    public void LoadPlayerPrefs()
+    {
+        PlayerPrefs.SetInt("TUTORIAL", inTutorialFinished);
+        PlayerPrefs.Save();
+
+        Debug.Log("PlayerPrefs 저장 완료");
+    }
+
+    public void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteKey("TUTORIAL");
+        LoadPlayerPrefs();
+
+        Debug.Log("PlayerPrefs 삭제 완료");
+    }
+
+}
+
+
+
