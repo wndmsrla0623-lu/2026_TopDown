56567cc1d38b34a163db978d832e316971e28732
wndmsrla0623-lu <wndmsrla0623@gmail.com>
2026-06-18 21:31:32 +0900

    타이머, 개수 화면 설정

--- 2026_Engine_TopDown/Assets/Script/GameManager.cs
+++ 2026_Engine_TopDown/Assets/Script/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     public string gameSceneName = "GameScene";
 
 
-    private void Awake()
+    private void Awake() 
     {
         if (Instance == null)
         {
